b0205effbe28620e43b527430351bddd4c8495bc
YJ Mao <ymao.mu@gmail.com>
2017-01-10 18:42:25 -0600

    read points

--- mesh.cpp
+++ mesh.cpp
@@ -1,13 +1,38 @@
 #include "mesh.h"
+#include <QFile>
+#include "openfoamqtextstream.h"
+#include <QTextEdit>
+#include <QMessageBox>
+#include <QDebug>
+
+const QString Mesh::boundaryFileName = "/boundary";
+const QString Mesh::faceFileName = "/faces";
+const QString Mesh::ownerFileName = "/owner";
+const QString Mesh::neighbourFileName = "/neighbour";
+const QString Mesh::pointFileName = "/points";
 
 Mesh::Mesh()
 {
 
 }
 
+Mesh::Mesh(QString & dir)
+{
+    readPoints(dir);
+    readFaces(dir);
+    readOwner(dir);
+    readNeighbour(dir);
+    readBoundary(dir);
+}
+
 Mesh::~Mesh()
 {
 
+}
+
+const QList<int> Mesh::faceNodes(int faceId) const
+{
+    return faceNode_[faceId];
 }
 
 const QList<int> Mesh::cellNodes(int cellId) const
@@ -18,4 +43,62 @@ const QList<int> Mesh::cellNodes(int cellId) const
 const QList<int> Mesh::cellFaces(int cellId) const
 {
     return cellFace_[cellId];
+}
+
+void Mesh::readPoints(QString &dir)
+{
+    QString fileName = dir + pointFileName;
+    QFile file(fileName);
+
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(NULL,"..", fileName+" fail to open!");
+        return;
+    }
+
+    OpenFOAMQTextStream in(&file);
+    in.ignore(QString("//"),2);
+
+    // size
+    int size;
+    in >> size;
+
+    qDebug() << "size: " << size;
+
+    in.ignore(QString("("),1);
+
+    for (int i = 0; i < size; i++) {
+        QString val;
+        double point[3];
+        for (int j = 0; j < 3; j++) {
+            in >> val;
+            val.remove(QRegExp("[()]"));
+//            val.remove(")");
+            point[j] = val.toDouble();
+
+        }
+//        qDebug() << point[0] << "\t" << point[1] << "\t" << point[2];
+        points_.push_back(QVector3D(point[0],point[1],point[2]));
+    }
+
+    file.close();
+}
+
+void Mesh::readFaces(QString &dir)
+{
+
+}
+
+void Mesh::readBoundary(QString &dir)
+{
+
+}
+
+void Mesh::readOwner(QString &dir)
+{
+
+}
+
+void Mesh::readNeighbour(QString &dir)
+{
+
 }
--- mesh.h
+++ mesh.h
@@ -2,22 +2,48 @@
 #define MESH_H
 
 #include <QList>
+#include <QString>
+#include <QVector3D>
 
 class Mesh
 {
 public:
+
+    static const QString boundaryFileName;
+    static const QString faceFileName;
+    static const QString ownerFileName;
+    static const QString neighbourFileName;
+    static const QString pointFileName;
+
+public:
     Mesh();
+    Mesh(QString&);
     ~Mesh();
 
 private:
-    QList<int> cells_; // a list of cell Id
-    QList<int> faces_; // a list of face Id
-    QList<int> nodes_; // a list of node Id
-    QList< QList<int> > cellFace_; // read from stream
-    QList< QList<int> > cellNode_; // read form stream
 
+    QList<QString> boundaryName_; // name of BC
+    QList<int> boundaryFaces_; //  faces
+    QList<int> faces_; // a list of face Id
+    double owners_; // owner of face
+    double neighbours_; // neighbor of face
+    QList<QVector3D> points_; // list of point
+
+    QList<int> cells_; // a list of cell Id
+
+    QList< QList<int> > faceNode_; // find node from face
+    QList< QList<int> > cellNode_; // find node from cell
+    QList< QList<int> > cellFace_; // find face from cell
+
+    const QList<int> faceNodes(int faceId) const; // return node
     const QList<int> cellNodes(int cellId) const; // return node
-    const QList<int> cellFaces(int cellId) const; // return node
+    const QList<int> cellFaces(int cellId) const; // return face
+
+    void readPoints(QString &dir);
+    void readFaces(QString &dir);
+    void readBoundary(QString &dir);
+    void readOwner(QString &dir);
+    void readNeighbour(QString &dir);
 
 };
 
