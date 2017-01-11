5d239d108bfc107808926a04ba6c14e64271782f
YJ Mao <ymao.mu@gmail.com>
2017-01-10 18:30:26 -0600

    openfoam mesh points now is readable

---
+++ openfoamqtextstream.cpp
@@ -0,0 +1,5 @@
+#include "openfoamqtextstream.h"
+
+OpenFOAMQTextStream::OpenFOAMQTextStream()
+{
+}
---
+++ openfoamqtextstream.h
@@ -0,0 +1,12 @@
+#ifndef OPENFOAMQTEXTSTREAM_H
+#define OPENFOAMQTEXTSTREAM_H
+
+#include <QTextStream>
+
+class OpenFOAMQTextStream : public QTextStream
+{
+public:
+    OpenFOAMQTextStream();
+};
+
+#endif // OPENFOAMQTEXTSTREAM_H
