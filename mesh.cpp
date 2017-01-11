2ccf09f2ac968aee41b3e1d36c54ccccc77211a7
YJ Mao <ymao.mu@gmail.com>
2017-01-11 17:27:46 -0600

    mesh now is a very complete class

--- mesh.cpp
+++ mesh.cpp
@@ -18,11 +18,47 @@ Mesh::Mesh()
 
 Mesh::Mesh(QString & dir)
 {
-    readPoints(dir);
-    readFaces(dir);
-    readOwner(dir);
-    readNeighbour(dir);
-    readBoundary(dir);
+    nCells_ = 0;
+    nFaces_ = 0;
+    nNodes_ = 0;
+
+    bool readState = readPoints(dir);
+    readState = readState && readFaces(dir);
+    readState = readState && readOwner(dir);
+    readState = readState && readNeighbour(dir);
+    readState = readState && readBoundary(dir);
+
+    if(readState) {
+        // find the total number of cell
+        nCells_ = *std::max_element(owner().begin(), owner().end()) + 1;
+        qDebug() << "========= Total Number of cell: =========" << nCells();
+        // find the total number of face
+        nFaces_ = faces().size();
+        nNodes_ = points().size();
+
+        createCellFaces();
+        createCellNodes();
+        createFaceNormals();
+
+        QList<double> px;
+        QList<double> py;
+        QList<double> pz;
+
+        QList<QVector3D>::const_iterator it;
+        for(it = points().begin(); it != points().end(); ++it) {
+            px.push_back(((*it).x()));
+            py.push_back(((*it).y()));
+            pz.push_back(((*it).z()));
+        }
+        box_.xlow = *std::min_element(px.begin(),px.end());
+        box_.xhig = *std::max_element(px.begin(),px.end());
+        box_.ylow = *std::min_element(py.begin(),py.end());
+        box_.yhig = *std::max_element(py.begin(),py.end());
+        box_.zlow = *std::min_element(pz.begin(),pz.end());
+        box_.zhig = *std::max_element(pz.begin(),pz.end());
+    }
+
+    meshState_ = readState;
 }
 
 Mesh::~Mesh()
@@ -32,7 +68,7 @@ Mesh::~Mesh()
 
 const QList<int> Mesh::faceNodes(int faceId) const
 {
-    return faceNode_[faceId];
+    return faces()[faceId];
 }
 
 const QList<int> Mesh::cellNodes(int cellId) const
@@ -45,14 +81,14 @@ const QList<int> Mesh::cellFaces(int cellId) const
     return cellFace_[cellId];
 }
 
-void Mesh::readPoints(QString &dir)
+bool Mesh::readPoints(QString &dir)
 {
     QString fileName = dir + pointFileName;
     QFile file(fileName);
 
     if(!file.open(QFile::ReadOnly | QFile::Text)) {
         QMessageBox::warning(NULL,"..", fileName+" fail to open!");
-        return;
+        return false;
     }
 
     OpenFOAMQTextStream in(&file);
@@ -62,7 +98,7 @@ void Mesh::readPoints(QString &dir)
     int size;
     in >> size;
 
-    qDebug() << "size: " << size;
+    qDebug() << "Node size: " << size;
 
     in.ignore(QString("("),1);
 
@@ -81,24 +117,272 @@ void Mesh::readPoints(QString &dir)
     }
 
     file.close();
+    return true;
 }
 
-void Mesh::readFaces(QString &dir)
+bool Mesh::readFaces(QString &dir)
 {
+    QString fileName = dir + faceFileName;
+    QFile file(fileName);
 
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(NULL,"..", fileName+" fail to open!");
+        return false;
+    }
+
+    OpenFOAMQTextStream in(&file);
+    in.ignore(QString("//"),2);
+
+    // size
+    int size;
+    in >> size;
+
+    qDebug() << "Face size: " << size;
+
+    in.ignore(QString("("),1);
+
+    for (int i = 0; i < size; i++) {
+        QString val;
+        in >> val;
+        QStringList str = val.split("(");
+        const int nVal = str[0].toInt();
+        int pIds[nVal];
+        QList<int> pIdsFace;
+
+        pIds[0] = str[1].toInt();
+        pIdsFace.push_back(pIds[0]);
+//        qDebug() << "nVal: " << nVal << " " << pIds[0];
+
+        for (int j = 1; j < nVal; j++) {
+            in >> val;
+            val.remove(QRegExp("[()]"));
+//            val.remove(")");
+            pIds[j] = val.toInt();
+            pIdsFace.push_back(pIds[j]);
+//            qDebug() << pIds[j];
+        }
+//        qDebug() << pIdsFace;
+        faces_.push_back(pIdsFace);
+    }
+
+    file.close();
+    return true;
 }
 
-void Mesh::readBoundary(QString &dir)
+bool Mesh::readBoundary(QString &dir)
 {
+    QString fileName = dir + boundaryFileName;
+    QFile file(fileName);
 
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(NULL,"..", fileName+" fail to open!");
+        return false;
+    }
+
+    OpenFOAMQTextStream in(&file);
+    in.ignore(QString("//"),2);
+
+    // size
+    int size;
+    in >> size;
+
+    qDebug() << "Boundary size: " << size;
+
+    in.ignore(QString("("),1);
+
+    QString boundaryName;
+    for (int i = 0; i < size; i++) {
+        in >> boundaryName;
+        in.ignore(QString("nFaces"),1); // until find the 1st string
+        int nFaces;
+        in >> nFaces;
+        in.ignore(QString("startFace"),1); // until find the 1st string
+        int startFace;
+        in >> startFace;
+        in.ignore(QString("}"),1);
+
+        QList<int> faceList;
+        for (int j = 0; j < nFaces; j++) {
+            faceList.push_back(startFace + j);
+        }
+        boundaryNameFaces_[boundaryName] = faceList;
+        boundaryName_.push_back(boundaryName);
+//        qDebug() << boundaryName << " " << boundaryNameFaces_[boundaryName];
+    }
+
+    file.close();
+    return true;
 }
 
-void Mesh::readOwner(QString &dir)
+bool Mesh::readOwner(QString &dir)
 {
+    QString fileName = dir + ownerFileName;
+    QFile file(fileName);
 
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(NULL,"..", fileName+" fail to open!");
+        return false;
+    }
+
+    OpenFOAMQTextStream in(&file);
+    in.ignore(QString("//"),2);
+
+    // size
+    int size;
+    in >> size;
+
+    qDebug() << "Owner size: " << size;
+
+    in.ignore(QString("("),1);
+
+    int cellId;
+    for (int i = 0; i < size; i++) {
+        in >> cellId;
+        owner_.push_back(cellId);
+//        qDebug() << QString("%1 : %2").arg(i+1).arg(cellId);
+    }
+
+    file.close();
+    return true;
 }
 
-void Mesh::readNeighbour(QString &dir)
+bool Mesh::readNeighbour(QString &dir)
 {
+    QString fileName = dir + neighbourFileName;
+    QFile file(fileName);
+
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(NULL,"..", fileName+" fail to open!");
+        return false;
+    }
+
+    OpenFOAMQTextStream in(&file);
+    in.ignore(QString("//"),2);
+
+    // size
+    int size;
+    in >> size;
+
+    qDebug() << "Neighbour size: " << size;
+
+    in.ignore(QString("("),1);
+
+    int cellId;
+    for (int i = 0; i < size; i++) {
+        in >> cellId;
+        neighbour_.push_back(cellId);
+//        qDebug() << QString("%1 : %2").arg(i+1).arg(cellId);
+    }
+
+    file.close();
+    return true;
+}
+
+void Mesh::createCellFaces()
+{
+    // create cell to face, using owner and neighbour
+    cellFace_.reserve(nCells());
+    for(int i = 0; i < nCells(); i++) {
+        QList<int> q;
+        cellFace_.append(q);
+    }
+
+    // get faces from owner
+    qDebug() << "Get faces from owner...";
+    QList<int>::const_iterator it;
+    int faceId = 0;
+    for (it = owner().begin(); it != owner().end(); ++it) {
+        int cellId = *it;
+//        qDebug() << cellId << " " << faceId;
+        QList<QList<int> >::iterator it2;
+        it2 = cellFace_.begin() + cellId;
+        (*it2).push_back(faceId);
+        faceId++;
+    }
+
+    // get faces from neighbour
+    qDebug() << "Get faces from neighbour...";
+    faceId = 0;
+    for (it = neighbour().begin(); it != neighbour().end(); ++it) {
+        int cellId = *it;
+//        qDebug() << cellId << " " << faceId;
+        QList<QList<int> >::iterator it2;
+        it2 = cellFace_.begin() + cellId;
+        (*it2).push_back(faceId);
+        faceId++;
+    }
+
+//    int cellId = 0;
+//    QList<QList<int> >::const_iterator it2;
+//    for(it2 = cellFace().begin(); it2 != cellFace().end(); ++it2){
+//        qDebug() << ++cellId <<" "<< (*it2);
+//    }
 
 }
+
+
+void Mesh::createCellNodes()
+{
+    // duplicate node
+    cellNode_.reserve(nCells());
+    for(int i = 0; i < nCells(); i++) {
+        QList<int> q;
+        cellNode_.append(q);
+    }
+
+    // loop over all cells
+    int cellId = 0;
+    for (cellId = 0; cellId < nCells(); cellId++) {
+        // find faceIds of cellId
+        const QList<int> & faceIds = cellFaces(cellId);
+        QList<int>::const_iterator it;
+//        qDebug() << "cellId: " << cellId << " " << faceIds;
+
+        QList<QList<int> >::iterator it2;
+        it2 = cellNode_.begin() + cellId;
+
+        for(it = faceIds.begin(); it != faceIds.end(); ++it) {
+            int faceId = *it;
+            QList<int>::const_iterator it3;
+            const QList<int> nodeIds = faceNodes(faceId);
+
+            for(it3 = nodeIds.begin(); it3 != nodeIds.end(); ++it3) {
+                (*it2).push_back((*it3));
+            }
+        }
+    }
+
+//    cellId = 0;
+//    QList<QList<int> >::const_iterator it2;
+//    for(it2 = cellNode().begin(); it2 != cellNode().end(); ++it2){
+//        qDebug() << ++cellId <<" "<< (*it2);
+//    }
+
+}
+
+void Mesh::createFaceNormals()
+{
+    // duplicate node
+    faceNormals_.reserve(nFaces());
+    for(int i = 0; i < nFaces(); i++) {
+        QVector3D v;
+        faceNormals_.append(v);
+    }
+
+    // loop over all faces
+    int faceId = 0;
+    for (faceId = 0; faceId < nFaces(); faceId++) {
+        // find faceIds of cellId
+        const QList<int> & nodeIds = faceNodes(faceId);
+        QList<int>::const_iterator it;
+        it = nodeIds.begin();
+        QVector3D p1 = points()[*it];
+        ++it;
+        QVector3D p2 = points()[*it];
+        ++it;
+        QVector3D p3 = points()[*it];
+        faceNormals_.push_back(QVector3D::normal(p1, p2, p3));
+    }
+}
+
+const QList<QList<int> > Mesh::cellNode() const { return cellNode_;}
--- mesh.h
+++ mesh.h
@@ -4,6 +4,16 @@
 #include <QList>
 #include <QString>
 #include <QVector3D>
+#include <QMap>
+
+struct bound {
+    double xlow;
+    double xhig;
+    double ylow;
+    double yhig;
+    double zlow;
+    double zhig;
+};
 
 class Mesh
 {
@@ -20,30 +30,64 @@ public:
     Mesh(QString&);
     ~Mesh();
 
+    const QList<int> faceNodes(int faceId) const; // return node
+    const QList<int> cellNodes(int cellId) const; // return node
+    const QList<int> cellFaces(int cellId) const; // return face
+
+    // inline function
+    inline int nCells() const { return nCells_; }
+    inline int nFaces() const { return nFaces_; }
+    inline int nNodes() const { return nNodes_; }
+    inline bool meshState() const { return meshState_; }
+
+    inline const QList<QList<int> > faces() const { return faces_; }
+    inline const QList<int> owner() const { return owner_; }
+    inline const QList<int> neighbour() const { return neighbour_; }
+    inline const QList<QVector3D> points() const { return points_; }
+    inline const QList<QVector3D> faceNormals() const { return faceNormals_; }
+    inline const QList<QString> boundaryName() \
+    const { return boundaryName_; }
+
+    inline const QMap<QString, QList<int> > boundaryNameFaces() \
+    const { return boundaryNameFaces_; }
+
+    inline const QList< QList<int> > faceNode() const  { return faces_;}
+    inline const QList< QList<int> > cellNode() const;
+    inline const QList< QList<int> > cellFace() const { return cellFace_;}
+
+    inline const bound box() const { return box_; }
+
 private:
 
     QList<QString> boundaryName_; // name of BC
-    QList<int> boundaryFaces_; //  faces
-    QList<int> faces_; // a list of face Id
-    double owners_; // owner of face
-    double neighbours_; // neighbor of face
+    QMap<QString, QList<int> > boundaryNameFaces_; //  a list of face id
+    QList< QList<int> > faces_; // a list of list of face Id
+    QList<int> owner_; // owner of face
+    QList<int> neighbour_; // neighbor of face
     QList<QVector3D> points_; // list of point
+    QList<QVector3D> faceNormals_; // normal of each face
 
-    QList<int> cells_; // a list of cell Id
+    int nCells_; // total number of cells
+    int nFaces_; // total number of faces
+    int nNodes_; // total number of nodes
+
+    bool meshState_;
 
     QList< QList<int> > faceNode_; // find node from face
     QList< QList<int> > cellNode_; // find node from cell
     QList< QList<int> > cellFace_; // find face from cell
 
-    const QList<int> faceNodes(int faceId) const; // return node
-    const QList<int> cellNodes(int cellId) const; // return node
-    const QList<int> cellFaces(int cellId) const; // return face
+    bool readPoints(QString &dir);
+    bool readFaces(QString &dir);
+    bool readBoundary(QString &dir);
+    bool readOwner(QString &dir);
+    bool readNeighbour(QString &dir);
 
-    void readPoints(QString &dir);
-    void readFaces(QString &dir);
-    void readBoundary(QString &dir);
-    void readOwner(QString &dir);
-    void readNeighbour(QString &dir);
+    void createCellFaces();
+    void createCellNodes();
+    void createFaceNormals();
+
+    bound box_;
 
 };
 
