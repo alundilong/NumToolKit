938829835a1dc445efcfc9dce3e6faf168f3831a
YJ Mao <ymao.mu@gmail.com>
2017-01-17 20:24:35 -0600

    Template Matrix Mathmatical operation added

--- mathextension.cpp
+++ mathextension.cpp
@@ -9,3 +9,32 @@ void mathExtension::matrixMultiplyScalar(double **m, int nrow, int ncol, double 
     }
 
 }
+
+template <typename T , size_t rows, size_t cols>
+void mathExtension::matrixMultiplyMatrix(T (&array1)[rows][cols], T(&array2)[rows][cols], T(&array)[rows][cols])
+{
+    for (size_t i = 0; i < rows; ++i)
+    {
+        for (size_t j = 0; j < cols; ++j) {
+            array1[i][j]*array2[j][i];
+        }
+    }
+}
+
+template <typename T , size_t rows, size_t cols>
+void mathExtension::matrixMultiplyVector(T (&array1)[rows][cols], T(&array2)[rows], T(&array)[rows])
+{
+
+}
+
+
+void mathExtension::matrixMultiplyMatrix(double **m, int nrow_m, int ncol_m, double **n, int nrow_n, int ncol_n)
+{
+
+}
+
+
+void mathExtension::matrixMultiplyVector(double **m, int nrow_m, int ncol_n, double *n, int nrow_n)
+{
+
+}
--- mathextension.h
+++ mathextension.h
@@ -1,13 +1,21 @@
 #ifndef MATHEXTENSION_H
 #define MATHEXTENSION_H
 
+#include <iostream>
+
 namespace mathExtension {
 
-void matrixMultiplyScalar(double **m, int nrow, int ncol, double s);
+void matrixMultiplyScalar(double **m, int nrow, int ncol, double s);// m will be modified
 
 void matrixMultiplyMatrix(double **m, int nrow_m, int ncol_m, double **n, int nrow_n, int ncol_n);
 
 void matrixMultiplyVector(double **m, int nrow_m, int ncol_n, double *n, int nrow_n);
+
+template <typename T , size_t rows, size_t cols>
+void matrixMultiplyMatrix(T (&array1)[rows][cols], T(&array2)[rows][cols], T(&array)[rows][cols]);
+
+template <typename T , size_t rows, size_t cols>
+void matrixMultiplyVector(T (&array1)[rows][cols], T(&array2)[rows], T(&array)[rows]);
 
 // gauss point integration
 
